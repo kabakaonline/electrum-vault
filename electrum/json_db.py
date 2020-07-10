2fc5443c53d766f22987c967789eefbe54dc7332
harzo <harzojunior@gmail.com>
2020-07-10 13:54:50 +0200

    Add txtype to json_db

--- electrum/json_db.py
+++ electrum/json_db.py
@@ -695,16 +695,17 @@ class JsonDB(Logger):
     def get_verified_tx(self, txid):
         if txid not in self.verified_tx:
             return None
-        height, timestamp, txpos, header_hash = self.verified_tx[txid]
+        height, timestamp, txpos, header_hash, txtype = self.verified_tx[txid]
         return TxMinedInfo(height=height,
                            conf=None,
                            timestamp=timestamp,
                            txpos=txpos,
-                           header_hash=header_hash)
+                           header_hash=header_hash,
+                           txtype=txtype)
 
     @modifier
     def add_verified_tx(self, txid, info):
-        self.verified_tx[txid] = (info.height, info.timestamp, info.txpos, info.header_hash)
+        self.verified_tx[txid] = (info.height, info.timestamp, info.txpos, info.header_hash, info.txtype)
 
     @modifier
     def remove_verified_tx(self, txid):
@@ -851,7 +852,7 @@ class JsonDB(Logger):
         self.transactions = self.get_data_ref('transactions')   # type: Dict[str, Transaction]
         self.spent_outpoints = self.get_data_ref('spent_outpoints')  # txid -> output_index -> next_txid
         self.history = self.get_data_ref('addr_history')  # address -> list of (txid, height)
-        self.verified_tx = self.get_data_ref('verified_tx3')  # txid -> (height, timestamp, txpos, header_hash)
+        self.verified_tx = self.get_data_ref('verified_tx3')  # txid -> (height, timestamp, txpos, header_hash, txtype)
         self.tx_fees = self.get_data_ref('tx_fees')  # type: Dict[str, TxFeesValue]
         # convert raw hex transactions to Transaction objects
         for tx_hash, raw_tx in self.transactions.items():
